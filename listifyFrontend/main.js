cd0222749a87b1ab6ef849a83d64e84293108d08
Jack Garritano <114269804+jackgarritano@users.noreply.github.com>
2023-03-22 16:19:35 -0500

    moved initForm helper functions outside of initForm function

--- listifyFrontend/main.js
+++ listifyFrontend/main.js
@@ -1,13 +1,8 @@
 import './style.css';
 import { getAllTasks } from './controller/fetch';
-import { sortOnLoad } from './controller/ordering';
-import { renderCalendar } from './views/calendar';
-import { onlyPasteText } from './controller/formHandler';
-import { renderForm } from './views/renderForm';
 import { initForm, renderAddButton } from './views/formViewHandler';
 import { renderTask } from './views/renderTask';
 import { addToList } from './controller/taskList';
-import { setTimers } from './controller/switchHandler';
 import { initLoginScreen } from './views/loginScreen';
 
 window.handleAuth = async (a) => {
