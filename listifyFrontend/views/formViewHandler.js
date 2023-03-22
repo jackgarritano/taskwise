c1395a6ad92c06f05182428e5040c4a1018a890d
Jack Garritano <114269804+jackgarritano@users.noreply.github.com>
2023-03-22 16:19:35 -0500

    moved initForm helper functions outside of initForm function

--- listifyFrontend/views/formViewHandler.js
+++ listifyFrontend/views/formViewHandler.js
@@ -1,174 +1,107 @@
 import { renderForm } from "./renderForm";
 import { clearCal, renderCalendar } from "./calendar";
-import { onlyPasteText, regPriorityButtonClicked, maxPriorityButtonClicked,
-    observeTextFields, validateTimeInputs, getErrorMessage, formSubmission} from "../controller/formHandler";
+import {
+    onlyPasteText, regPriorityButtonClicked, maxPriorityButtonClicked,
+    observeTextFields, validateTimeInputs, getErrorMessage, formSubmission
+} from "../controller/formHandler";
 import grayPlusSvg from '../assets/plusLogo.svg';
 import bluePlusSvg from '../assets/plusLogoBlue.svg';
-export {initForm, derenderForm, renderAddButton};
+export { initForm, derenderForm, renderAddButton };
 
 let formElements;
+let highestUnavailable;
 
-function initForm(){
-let highestUnavailable = 0;
+function initForm() {
+    highestUnavailable = 0;
 
-document.querySelector('.addButton').remove();
+    document.querySelector('.addButton').remove();
 
-onlyPasteText();
-formElements = renderForm();
-let allTasks = document.querySelector('.allTasks');
-allTasks.prepend(formElements.addForm);
-formElements.taskName.focus();
-renderCalendar(formElements.dueDate);
-observeTextFields(checkIfAddAllowed);
- 
+    onlyPasteText();
+    formElements = renderForm();
+    let allTasks = document.querySelector('.allTasks');
+    allTasks.prepend(formElements.addForm);
+    formElements.taskName.focus();
+    renderCalendar(formElements.dueDate);
+    observeTextFields(checkIfAddAllowed);
 
-function horizCenterPopups(){
-    for(let i=0; i<arguments.length; i++){
-        arguments[i].style.left = `calc(50% - ${arguments[i].clientWidth}px / 2)`;
-    }
-}
-horizCenterPopups(formElements.errorMessage);
-hideErrorMessage();
-
-formElements.dueDate.addEventListener('click', (e) => {
-    let calendarHolder = document.querySelector('.calendarHolder')
-    if(e.target.classList.contains('dateCircle') || e.target.parentElement.classList.contains('dateCircle')){
-        calendarHolder.classList.add('hidden');
-    }
-    else if(calendarHolder.classList.contains('hidden')){
-        calendarHolder.classList.toggle('hidden');
-        closePopupListener();
-    }
-})
-
-formElements.estTime.addEventListener('click', () => {
-    let estTime = document.querySelector('.timePickerHolder')
-    if(estTime.classList.contains('hidden')){
-        estTime.classList.toggle('hidden');
-        closePopupListener();
-    }
-})
-
-function disableMaxPriority(maxDisabled){
-    highestUnavailable = maxDisabled;
-    let maxButtons = formElements.maxPriority.querySelectorAll('.priorityPicker');
-    maxButtons.forEach((el)=>{
-        if(el.dataset.number <= maxDisabled){
-            el.classList.add('dimmed');
-        }
-        else{
-            el.classList.remove('dimmed');
-        }
-    })
-
-}
-
-formElements.priority.addEventListener('click', (e) => {
-    let priorityPicker = document.querySelectorAll('.priorityPickerHolder')[0];
-    if(regPriorityButtonClicked(e.target)){
-        disableMaxPriority(e.target.dataset.number - 1);
-        checkIfAddAllowed();
-        priorityPicker.classList.add('hidden');
-    }
-    else if(priorityPicker.classList.contains('hidden')){
-        priorityPicker.classList.toggle('hidden');
-        closePopupListener();
-    }
-})
-formElements.maxPriority.addEventListener('click', (e) => {
-    let maxPriorityPicker = document.querySelectorAll('.priorityPickerHolder')[1];
-    if(maxPriorityButtonClicked(e.target, highestUnavailable)){
-        maxPriorityPicker.classList.add('hidden');
-    }
-    else if(maxPriorityPicker.classList.contains('hidden')){
-        maxPriorityPicker.classList.toggle('hidden');
-        closePopupListener();
-    }
-})
-formElements.estTime.querySelectorAll('.textInput').forEach((el)=>{
-    el.addEventListener('focus', (e)=>{
-        e.target.select();
-})})
-
-function closePopupListener(e){
-    let popup;
-    let popups = document.querySelectorAll('.popup');
-    popups.forEach((item) => {
-        if(!item.classList.contains('hidden')){
-            popup = item;
-        }
-    })
-    let closePopup = function(e){ 
-        if (!popup.contains(e.target)) {
-            popup.classList.add('hidden');
-            removeClosePopupListener();
-        }
-    }
-    let removeClosePopupListener = function(){
-        document.removeEventListener('click', closePopup, true);
-    }
-    document.addEventListener('click', closePopup, true)
-}
-
-formElements.cancel.addEventListener('click', derenderForm);
-
-function checkIfAddAllowed(){
-    if(document.querySelector('input[name=taskName]').value != '' &&
-     document.querySelector('input[name=priInput]').value != '' &&
-     (formElements.dueDate.querySelector('input[name=dateInput]').value != '' ||
-     formElements.maxPriority.querySelector('input[name=maxPriInput]').value == '')){
-        formElements.addTask.classList.remove('dimmed');
-    }
-    else{
-        formElements.addTask.classList.add('dimmed');
-    }
-}
-function checkOnValueMutation(mutations){
-    mutations.forEach((el)=>{
-        if (el.type === 'attributes' && el.attributeName === 'value'){
-            checkIfAddAllowed();
-        }
-    })
-}
-
-const valueObserver =  new MutationObserver(checkOnValueMutation);
-valueObserver.observe(formElements.dueDate.querySelector('input[name=dateInput]'), {attributes: true});
-valueObserver.observe(formElements.maxPriority.querySelector('input[name=maxPriInput]'), {attributes: true});
-
-formElements.estTime.querySelectorAll('input').forEach((el)=>{
-    validateTimeInputs(el);
-})
-
-function showErrorMessage(message){
-    formElements.errorMessage.textContent = message;
     horizCenterPopups(formElements.errorMessage);
-    formElements.errorMessage.style.top = '100%';
-}
+    hideErrorMessage();
 
-function addTaskHoverErr(e){
-    if(e.target.classList.contains('dimmed')){
-        showErrorMessage(getErrorMessage());
-    }
-}
+    formElements.dueDate.addEventListener('click', (e) => {
+        let calendarHolder = document.querySelector('.calendarHolder')
+        if (e.target.classList.contains('dateCircle') || e.target.parentElement.classList.contains('dateCircle')) {
+            calendarHolder.classList.add('hidden');
+        }
+        else if (calendarHolder.classList.contains('hidden')) {
+            calendarHolder.classList.toggle('hidden');
+            closePopupListener();
+        }
+    })
 
-formElements.addTask.addEventListener('mouseover', addTaskHoverErr);
+    formElements.estTime.addEventListener('click', () => {
+        let estTime = document.querySelector('.timePickerHolder')
+        if (estTime.classList.contains('hidden')) {
+            estTime.classList.toggle('hidden');
+            closePopupListener();
+        }
+    })
 
-document.addEventListener('click', hideErrorMessage);
+    formElements.priority.addEventListener('click', (e) => {
+        let priorityPicker = document.querySelectorAll('.priorityPickerHolder')[0];
+        if (regPriorityButtonClicked(e.target)) {
+            disableMaxPriority(e.target.dataset.number - 1);
+            checkIfAddAllowed();
+            priorityPicker.classList.add('hidden');
+        }
+        else if (priorityPicker.classList.contains('hidden')) {
+            priorityPicker.classList.toggle('hidden');
+            closePopupListener();
+        }
+    })
+    formElements.maxPriority.addEventListener('click', (e) => {
+        let maxPriorityPicker = document.querySelectorAll('.priorityPickerHolder')[1];
+        if (maxPriorityButtonClicked(e.target, highestUnavailable)) {
+            maxPriorityPicker.classList.add('hidden');
+        }
+        else if (maxPriorityPicker.classList.contains('hidden')) {
+            maxPriorityPicker.classList.toggle('hidden');
+            closePopupListener();
+        }
+    })
+    formElements.estTime.querySelectorAll('.textInput').forEach((el) => {
+        el.addEventListener('focus', (e) => {
+            e.target.select();
+        })
+    })
 
-formElements.addForm.addEventListener('submit', formSubmission);
-}
+    formElements.cancel.addEventListener('click', derenderForm);
 
-function hideErrorMessage(){
+    const valueObserver = new MutationObserver(checkOnValueMutation);
+    valueObserver.observe(formElements.dueDate.querySelector('input[name=dateInput]'), { attributes: true });
+    valueObserver.observe(formElements.maxPriority.querySelector('input[name=maxPriInput]'), { attributes: true });
+
+    formElements.estTime.querySelectorAll('input').forEach((el) => {
+        validateTimeInputs(el);
+    })
+
+    formElements.addTask.addEventListener('mouseover', addTaskHoverErr);
+
+    document.addEventListener('click', hideErrorMessage);
+
+    formElements.addForm.addEventListener('submit', formSubmission);
+} //end of init fn
+
+function hideErrorMessage() {
     formElements.errorMessage.style.top = `calc(100% - ${formElements.errorMessage.clientHeight}px)`;
 }
 
-function derenderForm(){ //this needs to also render the addTask button in its place
+function derenderForm() { //this needs to also render the addTask button in its place
     document.removeEventListener('click', hideErrorMessage);
     document.querySelector('.addForm').remove();
     renderAddButton();
 }
 
-function renderAddButton(){
+function renderAddButton() {
     let addButton = document.createElement('div');
     let addLogo = document.createElement('img');
     let textSpan = document.createElement('span');
@@ -183,10 +116,80 @@ function renderAddButton(){
     document.querySelector('.allTasks').prepend(addButton);
 
     addButton.addEventListener('click', initForm);
-    addButton.addEventListener('mouseover', ()=>{
+    addButton.addEventListener('mouseover', () => {
         document.querySelector('.addLogo').setAttribute('src', bluePlusSvg);
     })
-    addButton.addEventListener('mouseout', ()=>{
+    addButton.addEventListener('mouseout', () => {
         document.querySelector('.addLogo').setAttribute('src', grayPlusSvg);
     })
+}
+
+function addTaskHoverErr(e) {
+    if (e.target.classList.contains('dimmed')) {
+        showErrorMessage(getErrorMessage());
+    }
+}
+
+function showErrorMessage(message) {
+    formElements.errorMessage.textContent = message;
+    horizCenterPopups(formElements.errorMessage);
+    formElements.errorMessage.style.top = '100%';
+}
+
+function checkIfAddAllowed() {
+    if (document.querySelector('input[name=taskName]').value != '' &&
+        document.querySelector('input[name=priInput]').value != '' &&
+        (formElements.dueDate.querySelector('input[name=dateInput]').value != '' ||
+            formElements.maxPriority.querySelector('input[name=maxPriInput]').value == '')) {
+        formElements.addTask.classList.remove('dimmed');
+    }
+    else {
+        formElements.addTask.classList.add('dimmed');
+    }
+}
+function checkOnValueMutation(mutations) {
+    mutations.forEach((el) => {
+        if (el.type === 'attributes' && el.attributeName === 'value') {
+            checkIfAddAllowed();
+        }
+    })
+}
+
+function closePopupListener(e) {
+    let popup;
+    let popups = document.querySelectorAll('.popup');
+    popups.forEach((item) => {
+        if (!item.classList.contains('hidden')) {
+            popup = item;
+        }
+    })
+    let closePopup = function (e) {
+        if (!popup.contains(e.target)) {
+            popup.classList.add('hidden');
+            removeClosePopupListener();
+        }
+    }
+    let removeClosePopupListener = function () {
+        document.removeEventListener('click', closePopup, true);
+    }
+    document.addEventListener('click', closePopup, true)
+}
+
+function disableMaxPriority(maxDisabled) {
+    highestUnavailable = maxDisabled;
+    let maxButtons = formElements.maxPriority.querySelectorAll('.priorityPicker');
+    maxButtons.forEach((el) => {
+        if (el.dataset.number <= maxDisabled) {
+            el.classList.add('dimmed');
+        }
+        else {
+            el.classList.remove('dimmed');
+        }
+    })
+}
+
+function horizCenterPopups() {
+    for (let i = 0; i < arguments.length; i++) {
+        arguments[i].style.left = `calc(50% - ${arguments[i].clientWidth}px / 2)`;
+    }
 }
