a08c48b168535fbaf8ee59b5b30cc8beefe70618
Jack Garritano <114269804+jackgarritano@users.noreply.github.com>
2023-03-27 11:46:08 -0500

    ordering now works in all cases

--- listifyFrontend/controller/ordering.js
+++ listifyFrontend/controller/ordering.js
@@ -1,13 +1,16 @@
 import { getAllTasks } from "./fetch";
+import { calculateCurrentPriority } from "../views/renderTask";
 export {sortOnLoad};
 
 async function sortOnLoad(){  //todo: priority field doesn't reflect current priority: need to calculate the priority for this, not use the field
     let allTasks = await getAllTasks();
     allTasks.sort((item1, item2) => {
-        if(parseInt(item1.priority) > parseInt(item2.priority)){
+        priority1 = calculateCurrentPriority(item1.priority, item2.switchTimes);
+        priority2 = calculateCurrentPriority(item2.priority, item2.switchTimes);
+        if(parseInt(priority1) > parseInt(priority2)){
             return -1;
         }
-        else if(parseInt(item1.priority) < parseInt(item2.priority)){
+        else if(parseInt(priority1) < parseInt(priority2)){
             return 1;
         }
         else if(Date.parse(item1.due) > Date.parse(item2.due)){
